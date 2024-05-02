0c6e7dc399cacac6f363c021caf6b2b34244d9ba
Joshua Arrazola <2230023@upv.edu.mx>
2024-05-01 20:08:55 -0600

    daily problem :)

---
+++ 2441_largestPositive/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <array>
+#include <set>
+#include <math.h>
+#include <iomanip>
+#include <bitset>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("03")
+#pragma GCC target ("avx")
+
+#define PI (3.141592);
+
+typedef long long ll;
+typedef long l;
+
+template <typename T>
+using vt = vector<T>;
+
+template <typename J, typename A>
+using hm = unordered_map<J, A>;
+
+template <typename R>
+using st = stack<R>;
+
+static vector<int> nums=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return vector<int>{};
+}();
+
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        unordered_set<int> st; 
+
+        int answ = INT32_MIN;
+
+        for(int & i : nums){
+            if(abs(i) > answ && st.find(i * (-1)) != st.end())
+                answ = abs(i);
+            
+            st.insert(i);
+        }
+        
+        return ((answ == INT32_MIN) ? -1 : answ);
+    }   
+};
+
+int main()
+{
+
+    return 0;
+}
