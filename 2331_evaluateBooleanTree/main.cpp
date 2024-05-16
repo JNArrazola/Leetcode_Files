82fe6797ef727451eec91bd0647fc175c9f8ad4c
Joshua Arrazola <2230023@upv.edu.mx>
2024-05-16 10:32:33 -0600

    16-04-2025

---
+++ 2331_evaluateBooleanTree/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <array>
+#include <set>
+#include <math.h>
+#include <iomanip>
+#include <bitset>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("03")
+#pragma GCC target ("avx")
+
+#define PI (3.141592);
+
+typedef long long ll;
+typedef long l;
+
+template <typename T>
+using vt = vector<T>;
+
+template <typename J, typename A>
+using hm = unordered_map<J, A>;
+
+template <typename R>
+using st = stack<R>;
+
+static vector<int> nums=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return vector<int>{};
+}();
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    bool backTracking(TreeNode *node){
+        if(!(node->left||node->right))
+            return ((node->val) ? true : false);
+        
+        return ((node->val == 2) ? backTracking(node->left)||backTracking(node->right) : backTracking(node->left)&&backTracking(node->right));
+    }
+
+public:
+    bool evaluateTree(TreeNode* root) {
+        return backTracking(root);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
